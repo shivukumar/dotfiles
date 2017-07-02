67ad4d86a586ee02e78275f32e0a38d130f6d273
shivukumar <shivukumarpatil1@gmail.com>
2017-07-01 19:30:23 -0700

    Added setup and ycm confg scripts

---
+++ setup.sh
@@ -0,0 +1,4 @@
+ln -s ~/dotfiles/vimrc ~/.vimrc
+ln -s ~/dotfiles/ycm_extra_conf.py ~/.ycm_extra_conf.py
+mkdir -p ~/.vim/bundle
+git clone https://github.com/VundleVim/Vundle.vim ~/.vim/bundle/Vundle.vim
